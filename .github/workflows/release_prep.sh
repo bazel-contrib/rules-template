37c8fb92a8f64eea2cc75dea8ccc1558f0bf8370
Alex Eagle <alex@aspect.dev>
2024-03-08 07:22:40 -0800

    chore: improve bzlmod install instructions
    it didn't account for Bazel 7

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -16,9 +16,9 @@ git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
 SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
 
 cat << EOF
-## Using Bzlmod with Bazel 6
+## Using Bzlmod with Bazel 6 or greater
 
-1. Enable with \`common --enable_bzlmod\` in \`.bazelrc\`.
+1. (Bazel 6 only) Enable with \`common --enable_bzlmod\` in \`.bazelrc\`.
 2. Add to your \`MODULE.bazel\` file:
 
 \`\`\`starlark
